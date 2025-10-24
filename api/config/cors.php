df82596846b4d75259ee0f1fa8ffb1bf494fc0bf
西岡浩喜 <nishiokahiroki@nishiokahirokinoMacBook-Air.local>
2025-10-24 21:36:33 +0900

    Add Render frontend domain to CORS allowed origins

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,7 +19,15 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3001', 'http://127.0.0.1:3001', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:3001',
+        'http://127.0.0.1:3001',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://pocket-money-frontend.onrender.com'
+    ],
 
     'allowed_origins_patterns' => [],
 
